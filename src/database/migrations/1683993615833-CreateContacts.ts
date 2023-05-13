acdceccc4e5df9009cbac400270e944d135ca4aa
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-05-13 13:12:37 -0300

    Criando migration de contatos

---
+++ src/database/migrations/1683993615833-CreateContacts.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateContacts1683993615833 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'contacts',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+
+          {
+            name: 'celular',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('contacts');
+  }
+}
