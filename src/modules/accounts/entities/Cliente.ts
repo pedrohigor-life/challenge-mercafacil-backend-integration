704171036c142385a85cba50b0fe4c4b58436410
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-05-13 17:38:49 -0300

    Criando entidade e migrations de clientes

---
+++ src/modules/accounts/entities/Cliente.ts
@@ -0,0 +1,32 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+
+import { v4 as uuidv4 } from 'uuid';
+
+export type NameClient = 'macapa' | 'varejao';
+
+@Entity('clients')
+class Client {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: ['macapa', 'varejao'],
+  })
+  client: NameClient;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) this.id = uuidv4();
+  }
+}
+
+export { Client };
