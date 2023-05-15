1a1b0d706fd6bb622ae520fc5e4ae600ca5d2d80
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-05-15 13:36:27 -0300

    Criando migrationde clientes no MySQL

---
+++ src/database/migrations/mysql/1684168068960-CreateContacts.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateContacts1684168068960 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    new Table({
+      name: 'clients',
+      columns: [
+        {
+          name: 'id',
+          type: 'string',
+          isPrimary: true,
+          isNullable: false,
+        },
+
+        {
+          name: 'email',
+          type: 'varchar',
+          isNullable: false,
+        },
+
+        {
+          name: 'password',
+          type: 'varchar',
+        },
+
+        {
+          name: 'client',
+          type: 'varchar',
+        },
+
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'now()',
+        },
+      ],
+    });
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('contacts');
+  }
+}
